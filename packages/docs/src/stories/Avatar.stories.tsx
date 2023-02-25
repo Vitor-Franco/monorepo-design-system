75800aae55d386f0ff6db906f226a43ca9821fb2
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-02-25 16:19:28 -0300

    feat(avatar): adiciona componente de avatar com fallback de imagem

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@travessia-ui/react'
+
+// Tipagens padrão do Componente
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/vitor-franco.png',
+    alt: 'Vitor Franco',
+  },
+} as Meta<AvatarProps>
+
+// Variações do componente
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
