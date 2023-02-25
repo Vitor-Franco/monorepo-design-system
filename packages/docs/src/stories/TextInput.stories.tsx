80b12e019460e176de77b2d091fb11b91638f54a
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-02-25 16:57:28 -0300

    feat(input): adiciona componente de input juntamente com suas variacoes

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,46 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@travessia-ui/react'
+
+// Tipagens padrão do Componente
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+// Variações do componente
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
